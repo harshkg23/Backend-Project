e61b7ec02f7a581f7d02c9b92d672d264bcec703
Harsh Kumar Gupta <harshkumargupta2023@gmail.com>
2024-11-29 22:28:38 +0530

    remove commented-out code from index.js

--- src/index.js
+++ src/index.js
@@ -25,37 +25,3 @@ connectDB()
 
 
 
-/*
-
-// 1st approach
-
-import express from "express";
-const app= express()
-
-(async ()=>{
-    try {
-        await mongoose.connect(`${process.env.MONDODB_URI}/${DB_NAME}`)
-        app.on("error", (error)=>{
-            console.log("ERROR:",error);
-            throw error
-            
-        })
-
-        app.listen(process.env.PORT,()=>{
-            console.log(`App is listening on port ${process.env.PORT}`);
-            
-        })
-
-
-    } catch (error) {
-        console.error("ERROR:",error)
-        throw err
-        
-    }
-})()
-    */
-
-
-
-
-
